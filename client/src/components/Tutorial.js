28631f247015b4fe519281e4a7211061ab73314c
Asim Riaz <asim.riaz@live.com>
2023-03-12 21:48:51 +0500

    Updates

--- client/src/components/Tutorial.js
+++ client/src/components/Tutorial.js
@@ -5,11 +5,12 @@ import "xterm/dist/xterm.css";
 import "./tutorial.css";
 import * as fit from "xterm/lib/addons/fit/fit";
 import "regenerator-runtime/runtime";
+import { cmds } from "./query";
 import axios from "axios";
 
 let terminal;
 
-const Tutorial = ({ created, filename, execute }) => {
+const Tutorial = () => {
     //let URL = `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`;
     const termRef = useRef();
     const targetRef = useRef();
@@ -28,7 +29,7 @@ const Tutorial = ({ created, filename, execute }) => {
             method: "POST",
         }).then((res) =>
             res.text().then(async (pid) => {
-                console.log(`new term pid >> ${pid}`);
+                console.log(`new pid >> ${pid}`);
                 setPid(pid);
                 sessionStorage.setItem("pid", pid);
                 establish(pid);
@@ -49,23 +50,15 @@ const Tutorial = ({ created, filename, execute }) => {
     useEffect(() => {
         console.log(`pid >> ${pid}`);
         if (dimensions.width !== 0) {
-            //console.log(`Terminal creation statrs ${dimensions.width}`);
             terminal = new Terminal({
                 cols: dimensions.width,
                 rows: dimensions.height,
                 cursorBlink: true,
                 rendererType: "dom",
-                // theme: {
-                //     background: "#f5f5f5",
-                //     foreground: "#000000",
-                //     cursor: "#000000",
-                // },
             });
 
             if (pid === 0) {
-                (async () => {
-                    await getTerm();
-                })();
+                (async () => await getTerm())();
             } else {
                 establish(pid);
             }
@@ -74,13 +67,13 @@ const Tutorial = ({ created, filename, execute }) => {
 
     const establish = (pid) => {
         let socket = new WebSocket(`ws://${window.location.host}/terminals/${pid}`);
-        console.log(socket.readyState);
+        //console.log(socket.readyState);
         terminal.loadAddon(new AttachAddon(socket));
         setTermSocket(socket);
         terminal.open(termRef.current);
         setTimeout(() => {
             socket.send("clear screen\n");
-        }, 1000);
+        }, 500);
     };
 
     const runCmd = (cmd) => {
@@ -93,109 +86,11 @@ const Tutorial = ({ created, filename, execute }) => {
     };
 
     let href = "#";
-    //let cmds = [`clear screen`, `SELECT * FROM tab;`, `SELECT * FROM emp;`, `LIST`, `/`];
-    //let cmds = [[`clear screen`], [`SELECT * `, `FROM tab;`], [`SELECT * `, `FROM emp;`], [`LIST`], [`/`]];
-    let cmds = [
-        [`clear screen`],
-        [`SELECT * `, `FROM tab;`],
-        [`SELECT * `, `FROM countries;`],
-        [`SELECT * `, `FROM departments;`],
-        [`SELECT * `, `FROM employees;`],
-        [`SELECT * `, `FROM emp_details_view;`],
-        [`SELECT * `, `FROM jobs;`],
-        [`SELECT * `, `FROM job_history;`],
-        [`SELECT * `, `FROM locations;`],
-        [`SELECT * `, `FROM regions;`],
-        [`SELECT last_name, job_id, department_id`, `FROM employees`, `WHERE last_name = 'Whalen' ;`],
-        [
-            `SELECT  AVG(salary), MAX(salary),`,
-            `        MIN(salary), SUM(salary)`,
-            `FROM employees`,
-            `WHERE job_id LIKE '%REP%';`,
-        ],
-        [`SELECT MIN(hire_date), MAX(hire_date)`, `FROM employees;`],
-        [`SELECT COUNT(*)`, `FROM employees`, `WHERE department_id = 50;`],
-        [`SELECT COUNT(commission_pct)`, `FROM employees`, `WHERE department_id = 80;`],
-        [`SELECT COUNT(DISTINCT department_id)`, `FROM employees;`],
-        [`SELECT AVG(commission_pct)`, `FROM employees;`],
-        [`SELECT AVG(NVL(commission_pct, 0))`, `FROM employees;`],
-        [`SELECT department_id, AVG(salary)`, `FROM employees`, `GROUP BY department_id`, `ORDER BY department_id ;`],
-        [`SELECT AVG(salary)`, `FROM employees`, `GROUP BY department_id ;`],
-        [
-            `SELECT department_id, job_id, SUM(salary)`,
-            `FROM employees`,
-            `WHERE department_id > 40`,
-            `GROUP BY department_id, job_id `,
-            `ORDER BY department_id;`,
-        ],
-        [
-            `SELECT department_id, MAX(salary)`,
-            `FROM employees`,
-            `GROUP BY department_id`,
-            `HAVING MAX(salary) > 10000 ;`,
-        ],
-        [
-            `SELECT job_id, SUM(salary) PAYROLL`,
-            `FROM employees`,
-            `WHERE job_id NOT LIKE '%REP%'`,
-            `GROUP BY job_id`,
-            `HAVING SUM(salary) > 13000`,
-            `ORDER BY SUM(salary);`,
-        ],
-        [`SELECT MAX(AVG(salary))`, `FROM employees`, `GROUP BY department_id;`],
-        [
-            `SELECT e.last_name, e.department_id, d.department_name`,
-            `FROM employees e LEFT OUTER JOIN departments d`,
-            `ON (e.department_id = d.department_id) ;`,
-        ],
-        [
-            `SELECT e.last_name, d.department_id, d.department_name`,
-            `FROM employees e RIGHT OUTER JOIN departments d`,
-            `ON (e.department_id = d.department_id) ;`,
-        ],
-        [
-            `SELECT e.last_name, d.department_id, d.department_name`,
-            `FROM employees e FULL OUTER JOIN departments d`,
-            `ON (e.department_id = d.department_id) ;`,
-        ],
-        [`SELECT last_name, department_name`, `FROM employees`, `CROSS JOIN departments ;`],
-        [
-            `SELECT last_name, salary`,
-            `FROM employees`,
-            `WHERE salary >`,
-            `        (SELECT salary`,
-            `        FROM employees`,
-            `        WHERE last_name = 'Abel');`,
-        ],
-        [
-            `SELECT last_name, job_id, salary`,
-            `FROM employees`,
-            `WHERE job_id = `,
-            `        (SELECT job_id FROM employees`,
-            `        WHERE first_name = 'Jonathon')`,
-            `AND salary >`,
-            `        (SELECT salary FROM employees`,
-            `        WHERE first_name = 'Jonathon');`,
-        ],
-        [
-            `SELECT last_name, job_id, salary`,
-            `FROM employees`,
-            `WHERE salary = `,
-            `        (SELECT MIN(salary)`,
-            `        FROM employees);`,
-        ],
-        [`LIST`],
-        [`/`],
-    ];
     return (
         <div className="flex-wrapper" ref={targetRef}>
             <div
                 className="item"
                 style={{ height: 250, display: "flex", justifyContent: "center", alignItems: "center" }}>
-                {/* <p>{dimensions.width}</p>
-                <p>{dimensions.height}</p> */}
-                {/* {cmds.map((cmd, i) => { */}
-                {/* //console.log(cmd); return ( */}
                 <div
                     style={{ cursor: "pointer" }}
                     onClick={() => {
@@ -253,8 +148,6 @@ const Tutorial = ({ created, filename, execute }) => {
                         />{" "}
                     </svg>
                 </div>
-                {/* ); */}
-                {/* })} */}
             </div>
             <div ref={termRef} id="terminal-container" className="item" style={{ flex: 1 }}></div>
         </div>
